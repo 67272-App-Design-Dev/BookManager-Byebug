bd1fc8dc630071407a7afcfdea1de1bd58e1503c
Edward Cho <eycho@andrew.cmu.edu>
2021-03-03 19:03:48 -0500

    db_schema date version format change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912030110) do
+ActiveRecord::Schema.define(version: 2017_09_12_030110) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
